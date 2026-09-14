7804cd7c10b337ab824bf9461de5bfa0120587f6
Arkanona <arkanona@gmail.com>
2026-09-14 12:09:44 +0200

    Restructure project as monorepo

---
+++ backend/app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors')
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
+
+require('dotenv').config()
+require('./config/db')
+
+const authRoutes = require('./routes/authRoutes')
+
+const corsOption = {
+    origin: 'http://localhost:3000'
+}
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+    message: { status: 429, error: 'Too many request'}
+})
+
+
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+        crossOriginResourcePolicy: { policy: "cross-origin" }
+    })
+)
+
+app.use(cors(corsOption))
+app.use(limiter)
+app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+app.use('/api/v1/auth', authRoutes)
+
+
+app.get('/', (req, res) => {
+    res.send("WildPeak")
+})
+
+app.listen(port, () => {
+    console.log(`Serveur démarré sur http://localhost:${port}`)
+})
